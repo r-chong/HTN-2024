43c195788618f54ecdf92fc32bf176fcbb6e195e
Reese Chong <dev.reese.chong@gmail.com>
2024-09-14 19:57:00 -0400

    feat: object collision + typewriter

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
 
-    public Rigidbody theRB;
+    public Rigidbody rb;
     public float moveSpeed, jumpForce;
     private Vector2 moveInput;
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        Debug.Log("collision occurred");
     }
+
+    // private void InteractWithTypewriter(GameObject typewriter) {
+    //     Debug.Log("Interacting with typewriter. Activate typing UI here");
+    // }
 
     // Update is called once per frame
     void Update()
@@ -22,6 +26,6 @@ public class PlayerController : MonoBehaviour
         moveInput.y = Input.GetAxis("Vertical");
         moveInput.Normalize();
 
-        theRB.velocity = new Vector3(moveInput.x * moveSpeed, theRB.velocity.y, moveInput.y * moveSpeed);
+        rb.velocity = new Vector3(moveInput.x * moveSpeed, rb.velocity.y, moveInput.y * moveSpeed);
     }
 }
