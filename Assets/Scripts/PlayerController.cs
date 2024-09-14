eeb1dd02be2ddb25e9d5cd08f9e5dc6f03d3469d
Reese Chong <dev.reese.chong@gmail.com>
2024-09-14 17:08:00 -0400

    feat: player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public Rigidbody theRB;
+    public float moveSpeed, jumpForce;
+    private Vector2 moveInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+        moveInput.Normalize();
+
+        theRB.velocity = new Vector3(moveInput.x * moveSpeed, theRB.velocity.y, moveInput.y * moveSpeed);
     }
 }
